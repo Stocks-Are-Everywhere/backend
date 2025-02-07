71388a3711c63e9f408902fe49d35980e86861b9
HoSungChoo <hosung0610@naver.com>
2025-02-07 16:53:27 +0900

    DOCS

---
+++ src/main/java/org/scoula/soon_two_people/SoonTwoPeopleApplication.java
@@ -0,0 +1,13 @@
+package org.scoula.soon_two_people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoonTwoPeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoonTwoPeopleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/scoula/soon_two_people/SoonTwoPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scoula.soon_two_people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoonTwoPeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
