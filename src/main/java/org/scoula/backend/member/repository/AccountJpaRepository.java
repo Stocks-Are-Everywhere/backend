f0e8bb5e47005132c5d132d16d907e21b2eb1af4
HminiL <hmin10580@gmail.com>
2025-03-06 10:03:57 +0900

    merge: merge with dev

--- src/main/java/org/scoula/backend/member/repository/AccountJpaRepository.java
+++ src/main/java/org/scoula/backend/member/repository/AccountJpaRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AccountJpaRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findByMember(Member member);
     Optional<Account> findByMemberId(Long memberId);
+    Optional<Account> findByMember(Member member);
 }
